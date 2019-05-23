fd321c5c64964690c4ce65335420aba89419c95e
Антон Блохин <blokhin@blokhin>
2019-05-23 17:54:05 +0300

    Наброски списка навостей

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import ShortHews from 'src/ui/news/short';
 
-export const MainPage = () => (
-  <h1>MainPage</h1>
-);
+const testData = [
+  {
+    id: '1',
+    title: 'title_1 title_1title_1title_1title 1title_1title_1title_1title 1title_1title_1title_1title_1title 1title_1title_1title_1title_1title_1title_1title_1title_1',
+    previewText: 'string',
+    tags: [
+      {
+        id: 1,
+        text: 'string',
+      }
+    ]
+  }
+]
 
+export const MainPage = () => {
+  return <div>
+    {
+      testData.map(
+        (news) => <ShortHews key={news.id} {...news}/>
+      )
+    }
+  </div>
+  
+};
