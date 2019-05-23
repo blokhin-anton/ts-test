a1b1c05abccb2d3df04e399de1b583f3b5e121e6
Антон Блохин <blokhin@blokhin>
2019-05-23 16:41:15 +0300

    Подключение шаблона приложения

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import {Switch, Route} from 'react-router-dom';
 
+import Template from 'src/template/app';
+
 import {routes} from 'src/routes';
 
 export default () => {
   return (
-    <Switch>
-      {
-        routes.map(route => (
-          <Route
-            key={route.name}
-            exact={route.isExact}
-            path={route.path}
-            component={route.component}
-          />
-        ))
-      }
-    </Switch>
+    <Template>
+      <Switch>
+        {
+          routes.map(route => (
+            <Route
+              key={route.name}
+              exact={route.isExact}
+              path={route.path}
+              component={route.component}
+            />
+          ))
+        }
+      </Switch>
+    </Template>
   );
 }
